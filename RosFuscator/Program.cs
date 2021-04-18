6b407a9da53ef8b9ebec43194c2823c332ad0e4c
Flaaangvik <melvin@flangvik.com>
2021-04-18 22:30:44 +0200

    Now can obfuscate a wholw project / like seatbelt

--- RosFuscator/Program.cs
+++ RosFuscator/Program.cs
@@ -6,7 +6,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace RosFuscator
 {
@@ -52,7 +51,9 @@ namespace RosFuscator
     {
         public static string[] BadStrings = new string[] { "DllImport", "const" };
 
-        public List<String> _strings = new List<string>();
+        public static SyntaxKind[] BadTypes = new SyntaxKind[] { SyntaxKind.Parameter, SyntaxKind.ParameterList, SyntaxKind.CaseKeyword, SyntaxKind.CasePatternSwitchLabel, SyntaxKind.CaseSwitchLabel };
+
+        public List<(string varData, string fullString)> _stringDict = new List<(string varData, string fullString)>();
 
         public override void VisitLiteralExpression(LiteralExpressionSyntax node)
         {
@@ -67,14 +68,27 @@ namespace RosFuscator
                 {
 
                 }
+                if (((string)node.Token.Value).Contains("\\Users\\"))
+                {
+                    var value3 = (node.Parent.Parent.Parent.ToFullString());
+                    var value2 = (node.Parent.Parent.ToFullString());
+                    var value1 = (node.Parent.ToFullString());
+                    var fpp = "nar";
+                }
 
-                _strings.Add((string)node.Token.Value);
-
+                if (!BadTypes.Contains(node.Parent.Parent.Parent.Kind()) && !BadTypes.Contains(node.Parent.Parent.Kind()) && !BadTypes.Contains(node.Parent.Kind()))
+                    _stringDict.Add(((string)node.Token.Value, (string)node.Parent.Parent.ToFullString()));
+                else
+                    Console.WriteLine($"[+] Bad string value {(string)node.Token.Value}");
 
             }
 
+
+
         }
+
     }
+
 
     class Program
     {
@@ -114,60 +128,84 @@ namespace RosFuscator
 
             // Read some input source code
             string orgSourceCode = "";
-            string sourceCodePath = "";
+            string sourceCodeDirectory = @"C:\Users\FlangvikStream\Desktop\Seatbelt";
 
             if (args.Count() > 0)
-            {
-                sourceCodePath = args[0];
+                sourceCodeDirectory = args[0];
 
-                if (!File.Exists(sourceCodePath))
+
+
+            foreach (var orgSourceCodePath in Directory.EnumerateFiles(sourceCodeDirectory, "*.cs", SearchOption.AllDirectories).Where(x => !x.Contains(@"\obj\") && !x.Contains("AssemblyInfo.cs")))
+            {
+
+                Console.WriteLine($"[+] Obfuscating {orgSourceCodePath}");
+
+                orgSourceCode = File.ReadAllText(orgSourceCodePath);
+
+                //If this is Program.cs
+                if (orgSourceCodePath.EndsWith("Program.cs"))
                 {
-                    Console.WriteLine("[+] Missing input source code path , eg rosfuscator.exe Program.cs");
-                    Environment.Exit(0);
-                }
-            }
-
-            orgSourceCode = File.ReadAllText(sourceCodePath);
-
-            // Pull strings and XOOOOOR them
-            SyntaxTree tree = CSharpSyntaxTree.ParseText(orgSourceCode);
-            CompilationUnitSyntax root = tree.GetCompilationUnitRoot();
-
-
-
-            // FOOBAR -> base64 
-            var stringCollector = new StringsCollector();
-            stringCollector.Visit(root);
-            var newSourceCode = orgSourceCode;
-
-            foreach (var stringItem in stringCollector._strings.Where(x => !string.IsNullOrEmpty(x) && x.Length > 3 && !x.Equals("ABCDEFGHIJKLMNOPQRSTUVWXYZ")))
+                    var orgCode = @"    public static class Program
+    {";
+                    //We need to add our function
+                    orgSourceCode = orgSourceCode.Replace(orgCode,
+                        @"    public static class Program
+    {
+        public static byte[] xorEncDec(byte[] input, string theKeystring)
+        {
+            byte[] theKey = System.Text.Encoding.UTF8.GetBytes(theKeystring);
+            byte[] mixed = new byte[input.Length];
+            for (int i = 0; i < input.Length; i++)
             {
-                //For each string
-
-                //Generate an random key 
-                var randomkey = Guid.NewGuid().ToString();
-
-                var xorEncString = xorEncDec(Encoding.UTF8.GetBytes(stringItem), randomkey);
-
-                var xorEncStringB64 = Convert.ToBase64String(xorEncString);
-
-                var decrypted = xorEncDec(Convert.FromBase64String(xorEncStringB64), randomkey);
-
-                var decryptedLogic = $" Encoding.UTF8.GetString(xorEncDec(Convert.FromBase64String(\"{xorEncStringB64}\"), \"{randomkey}\"))";
-                //XOR the value
-
-                Console.WriteLine($" {stringItem} <=> {decryptedLogic}");
-
-                //Replace the original value with decrypt logic
-                newSourceCode = newSourceCode.Replace("@\"" + stringItem + "\"", decryptedLogic);
-                newSourceCode = newSourceCode.Replace("\"" + stringItem + "\"", decryptedLogic);
+                mixed[i] = (byte)(input[i] ^ theKey[i % theKey.Length]);
             }
+            return mixed;
+        }"
+                        );
+                }
 
-            var orgFilenName = Path.GetFileNameWithoutExtension(sourceCodePath);
-            var newPath = sourceCodePath.Replace(orgFilenName, "Obf" + orgFilenName);
-            Console.WriteLine($"[+] Obfuscated source written {newPath}");
-            File.WriteAllText(newPath, newSourceCode);
+                // Pull strings and XOOOOOR them
+                SyntaxTree tree = CSharpSyntaxTree.ParseText(orgSourceCode);
+                CompilationUnitSyntax root = tree.GetCompilationUnitRoot();
 
+
+
+                // FOOBAR -> base64 
+                var stringCollector = new StringsCollector();
+                stringCollector.Visit(root);
+                var newSourceCode = orgSourceCode;
+
+                foreach (var stringItem in stringCollector._stringDict.Where(x => !string.IsNullOrEmpty(x.varData) && x.varData.Length > 3 && !x.varData.Equals("ABCDEFGHIJKLMNOPQRSTUVWXYZ")))
+                {
+                    //For each string
+
+                    //Generate an random key 
+                    var randomkey = Guid.NewGuid().ToString();
+
+                    var xorEncString = xorEncDec(Encoding.UTF8.GetBytes(stringItem.varData), randomkey);
+
+                    var xorEncStringB64 = Convert.ToBase64String(xorEncString);
+
+                    var decrypted = xorEncDec(Convert.FromBase64String(xorEncStringB64), randomkey);
+
+                    var decryptedLogic = $"System.Text.Encoding.UTF8.GetString(Program.xorEncDec(System.Convert.FromBase64String(\"{xorEncStringB64}\"), \"{randomkey}\"))";
+                    //XOR the value
+
+                    //Console.WriteLine($" {stringItem} <=> {decryptedLogic}");
+
+                    //Replace the original value with decrypt logic
+                    var newSnippet = stringItem.fullString.Replace("@\"" + stringItem.varData + "\"", decryptedLogic);
+                    newSnippet = newSnippet.Replace("\"" + stringItem.varData + "\"", decryptedLogic);
+
+                    newSourceCode = newSourceCode.Replace(stringItem.fullString, newSnippet);
+
+                }
+
+                //var orgFilenName = Path.GetFileNameWithoutExtension(sourceCodeDirectory);
+                //var newPath = sourceCodeDirectory.Replace(orgFilenName, "Obf" + orgFilenName);
+                //Console.WriteLine($"[+] Obfuscated source written {newPath}");
+                File.WriteAllText(orgSourceCodePath, newSourceCode);
+            }
             // Save and compile newly obfuscated source code
 
 
